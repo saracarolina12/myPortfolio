daaa6067fe1ee8a185d878e7ab66861110508c07
sara_carolina12 <saracarogode@gmail.com>
2022-07-16 22:10:49 -0500

    'css-scrollbar0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import About from './components/About';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Header/>
+      <About/>
     </div>
   );
 }
