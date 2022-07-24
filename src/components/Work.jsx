6d59a323db63d1124540cc759064339ace5d41b5
sara_carolina12 <saracarogode@gmail.com>
2022-07-24 15:19:36 -0500

    complete work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,31 +3,38 @@ import React from 'react'
 function Work() {
     return (
         <>
-        <div id='main' className='row'>
-            <div className='column vertical-center center upper'>
-                <img className='enterprises-images' src={ require('../images/Kindynos.png') } />
-                <p className='enterprises'>Kindynos</p>
-                <hr className='horizontal-line'/>
-                <p className='date-work'>November 2021 - February 2022</p>
-                <a target="_blank" href='https://kindynos.mx' className='link reduce-top'><p>https://kindynos.mx/</p></a>
-                <p className='job-description'>• Worked in different areas such as smart contracts development and front/back-end services.</p>
-                <p className='technologies'>Go, Vue, Javascript, React.js, node.js</p>
-                
+        
+        <div id='main' className='row center-up'>
+            <div className='no-margin center title-container'>
+                <p className='title-center-text no-margin'>work()</p>
+                <p className='title-background no-margin'>WORK</p>
             </div>
-            <div className='column vertical-center center'>
-                <img className='enterprises-images' src={ require('../images/Vibing.png') } />
-                <p className='enterprises'>Vibing Studios</p>
-                <hr className='horizontal-line'/>
-                <p className='date-work'>March 2022 - Present</p>
-                <a target="_blank" href='https://www.linkedin.com/company/vibingstudios/about/'><p className='link'>https://www.linkedin.com/company/vibingstudios/about</p></a>
-                <p className='job-description'> • Work in back-end to sign and verify       
-                    and Ethereum addresses.<br/>
-                    • Built database with Solana and   
-                    Ethereum addresses.<br/>
-                    • Developed front-end using CSS for 
-                    Bitmon Launcher
-                </p>
-                <p className='technologies'>Typescript, React.js, next.js, Tailwind CSS, SQLite, node.js</p>
+            <div className='row center-up'>
+                <div className='column vertical-center center upper'>
+                    <img className='enterprises-images' src={ require('../images/Kindynos.png') } />
+                    <p className='enterprises'>Kindynos</p>
+                    <hr className='horizontal-line'/>
+                    <p className='date-work'>November 2021 - February 2022</p>
+                    <a target="_blank" href='https://kindynos.mx' className='link reduce-top'><p>https://kindynos.mx/</p></a>
+                    <p className='job-description'>• Worked in different areas such as smart contracts development and front/back-end services.</p>
+                    <p className='technologies'>Go, Vue, Javascript, React.js, node.js</p>
+                    
+                </div>
+                <div className='column vertical-center center'>
+                    <img className='enterprises-images' src={ require('../images/Vibing.png') } />
+                    <p className='enterprises'>Vibing Studios</p>
+                    <hr className='horizontal-line'/>
+                    <p className='date-work'>March 2022 - Present</p>
+                    <a target="_blank" href='https://www.linkedin.com/company/vibingstudios/about/'><p className='link'>https://www.linkedin.com/company/vibingstudios/about</p></a>
+                    <p className='job-description'> • Work in back-end to sign and verify       
+                        and Ethereum addresses.<br/>
+                        • Built database with Solana and   
+                        Ethereum addresses.<br/>
+                        • Developed front-end using CSS for 
+                        Bitmon Launcher
+                    </p>
+                    <p className='technologies'>Typescript, React.js, next.js, Tailwind CSS, SQLite, node.js</p>
+                </div>
             </div>
         </div>
     </>
